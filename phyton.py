8188b1598d58a6eccd4aa4afba3b7b6ac46aff76
JeantheCreator <jeansantos.emp@gmail.com>
2022-08-03 22:56:29 -0300

    conta simples

--- phyton.py
+++ phyton.py
@@ -1,5 +1,11 @@
-#Conta simples
-numero1 = int(input("digite um numero"))
+# Conta simples
+numero1 = int(input("digite um numero: "))
+numero2 = int(input("Digite mais um numero: "))
 
+soma = numero1 + numero2
+subtracao = numero1 - numero2
+divisao = numero1 / numero2
+multiplicacao = numero1 * numero2
 
-
+print("A soma é {}\nA subtração é {}\nA divisão é {}\nA Multiplicação é {}\n".format(
+    soma, subtracao, divisao, multiplicacao))
